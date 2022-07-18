03b45202901af174db14d1135aed9e0cbb317914
Brent Johnson <brentleejohnson73@gmail.com>
2022-07-18 18:09:20 +0200

    Finished

--- scripts/index.js
+++ scripts/index.js
@@ -12,5 +12,13 @@ function blurring() {
     clearInterval(int);
   }
 
-  console.log(load);
+  loadText.innerHTML = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+// FUNCTION FROM STACKOVERFLOW
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+function scale(number, inMin, inMax, outMin, outMax) {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
 }
